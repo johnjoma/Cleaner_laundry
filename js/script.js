466eb8066f3aa0d022790374509eaa5566252e78
johnjoma <irungujohnkimani@gmail.com>
2020-02-27 11:04:31 +0300

    Cleaner

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
     $("a").on('click', function (event) {
 
         // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
+        if (this.hash !== "#") {
             // Prevent default anchor click behavior
             event.preventDefault();
 
